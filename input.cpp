9e0648fab693be4cfe9ac5c4e6aa708a4dc91799
Mariana <mariana.izumiks@gmail.com>
2022-12-08 08:24:35 +0100

    Exercicio de input

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Type your number: ";
+    cin >> x; 
+    cout << "Your number is: " << x;
+}
+
+
+//cout << Gerar e/ou imprimir valores
+//cin >> Pega a entrada digitada
