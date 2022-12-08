af523708b11d0d27eec6f57224b8ed19014fe017
Mariana <mariana.izumiks@gmail.com>
2022-12-08 08:45:18 +0100

    Diferenca entre float e double

--- tiposVar.cpp
+++ tiposVar.cpp
@@ -2,5 +2,10 @@
 usign namespace std;
 
 int main(){
-
+    int num = 2;                 //Numeros inteiros, positivos e negativos
+    float floatNum = 3.55;       //Pontos flutuantes
+    double doubleNum = 9.98;     //Pontos flutuantes, ocupa mais espaco na memoria, armazena numeros maiores
+    char letter = 'M';           //Caracteres unicos
+    bool boolean = true;         //Verdadeiro ou falso
+    string text = "Eae mundo";   //Textos sem tamanho limite
 }
